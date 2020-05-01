4919c7c5273bdc36dee787585a8bf12390b82020
Matthew Abraham <matthewabraham@localhost.localdomain>
2020-05-01 17:17:59 -0400

    Restructure for multiple learning activities.

---
+++ js/deferanimation.js
@@ -0,0 +1,7 @@
+document.body.className += " js-loading";
+
+window.addEventListener("load", showPage, false);
+
+function showPage() {
+  document.body.className = document.body.className.replace("js-loading", "");
+}
